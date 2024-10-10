3d0224a89baac0f4b54416f49f498edf2fee08b7
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-11 00:19:39 +0530

    [vaibhav]added: first commit

---
+++ src/config/database.ts
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+import Logger from './logger';
+
+class Database {
+  private DATABASE: string;
+  private logger;
+
+  constructor() {
+    this.DATABASE =
+      process.env.NODE_ENV === 'test'
+        ? process.env.DATABASE_TEST
+        : process.env.DATABASE_URI;
+
+    this.logger = Logger.logger;
+  }
+
+  public initializeDatabase = async (): Promise<void> => {
+    try {
+      await mongoose.connect(this.DATABASE, {
+        useFindAndModify: false,
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+      this.logger.info('Connected to the database.');
+    } catch (error) {
+      this.logger.error('Could not connect to the database.', error);
+    }
+  };
+}
+export default Database;
