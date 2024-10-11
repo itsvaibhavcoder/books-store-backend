8aff588e0a386a0e72058b68a02950f7cb3994fb
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-11 13:51:44 +0530

    [vaibhav]added:service for login user

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,6 @@
 import User from '../models/user.model';
 import { IUser } from '../interfaces/user.interface';
-
+import bcrypt from 'bcrypt';
 class UserService {
 
   //Create new user (Admin or Customer)
@@ -12,6 +12,20 @@ class UserService {
     const data = await User.create(body);
     return data;
   };
+
+  //Log in user
+  public login = async(email: string, password:string): Promise<any>=>{
+    const user = await User.findOne({email}).exec();
+    if(user){
+      const validate = await bcrypt.compare(password, user.password);
+      if(validate){
+        return user;
+      }
+      return false;
+    }
+    return false;
+  };
+
 }
 
 export default UserService;
