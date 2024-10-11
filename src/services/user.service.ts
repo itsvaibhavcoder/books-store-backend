116e7175bebb9e24b420e2744f711d650053b9e0
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-11 11:40:06 +0530

    [vaibhav]added:service for signup user/admin

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,41 +3,13 @@ import { IUser } from '../interfaces/user.interface';
 
 class UserService {
 
-  //get all users
-  public getAllUsers = async (): Promise<IUser[]> => {
-    const data = await User.find();
-    return data;
-  };
-
-  //create new user
-  public newUser = async (body: IUser): Promise<IUser> => {
+  //Create new user (Admin or Customer)
+  public signUp = async (body: IUser): Promise<IUser> => {
+    const existingUser = await User.findOne({email: body.email});
+    if(existingUser){
+      throw new Error('Email already exists');
+    }
     const data = await User.create(body);
-    return data;
-  };
-
-  //update a user
-  public updateUser = async (_id: string, body: IUser): Promise<IUser> => {
-    const data = await User.findByIdAndUpdate(
-      {
-        _id
-      },
-      body,
-      {
-        new: true
-      }
-    );
-    return data;
-  };
-
-  //delete a user
-  public deleteUser = async (_id: string): Promise<string> => {
-    await User.findByIdAndDelete(_id);
-    return '';
-  };
-
-  //get a single user
-  public getUser = async (_id: string): Promise<IUser> => {
-    const data = await User.findById(_id);
     return data;
   };
 }
