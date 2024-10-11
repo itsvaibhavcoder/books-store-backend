d1809ca7226d2df92e5a84b3e468dcaaac57a12e
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-11 14:54:18 +0530

    [vaibhav]added: generate token and reset token

--- src/utils/tokenUtils.ts
+++ src/utils/tokenUtils.ts
@@ -7,3 +7,13 @@ export const generateToken = (payload: { UserID: string, email: string }): strin
     const secretKey = process.env.JWT_SECRET_KEY as string;
     return jwt.sign(payload, secretKey, { expiresIn: '1h' });
 };
+
+export const generateResetToken = (email: string): string => {
+    const secretKeyForResetPassword = process.env.JWT_SECRET_KEY_FOR_RESET_PASSWORD as string;
+    return jwt.sign({ email }, secretKeyForResetPassword, { expiresIn: '1h' });
+};
+
+export const verifyResetToken = (token: string): any => {
+    const secretKeyForverify = process.env.JWT_SECRET_KEY_FOR_RESET_PASSWORD as string;
+    return jwt.verify(token, secretKeyForverify);
+};
