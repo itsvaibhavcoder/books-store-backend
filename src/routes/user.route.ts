0853479b98800a60adbd445c2857e60042919302
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-11 13:52:20 +0530

    [vaibhav]added:routes for login user

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -2,30 +2,40 @@ import express, { IRouter } from 'express';
 import userController from '../controllers/user.controller';
 import userValidator from '../validators/user.validator';
 import { handleRole } from '../middlewares/handleRole.middleware';
-
+import hashingFunction from '../hashing';
 class UserRoutes {
   private UserController = new userController();
   private router = express.Router();
   private UserValidator = new userValidator();
-
+  private Hashing = new hashingFunction();
   constructor() {
     this.routes();
   }
 
   private routes = () => {
+    //Customer Registration
     this.router.post(
       '',
       this.UserValidator.signUpValidate,
+      this.Hashing.getEncryptMiddleware(),
       handleRole,
       this.UserController.signUp
     ),
-    
+
+    //Admin registration
     this.router.post(
       '/adminsignup',
       this.UserValidator.signUpValidate,
+      this.Hashing.getEncryptMiddleware(),
       handleRole,
       this.UserController.signUp
     );
+
+    this.router.post(
+      '/signin',
+      this.UserValidator.loginValidate,
+      this.UserController.login
+    )
   };
 
   public getRoutes = (): IRouter => {
