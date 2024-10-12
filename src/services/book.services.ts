06af6f26eda6931abb73e6c0f9e47adc745da7b1
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-12 11:23:08 +0530

    [vaibhav]added: service for update and delete book

--- src/services/book.services.ts
+++ src/services/book.services.ts
@@ -4,12 +4,12 @@ import User from '../models/user.model';
 
 class BookService {
   constructor() {
-    console.log("BookService instantiated...");
+    console.log('BookService instantiated...');
   }
 
   // Book service for adding a book
   public addBook = async (body: IBook): Promise<IBook> => {
-    console.log("Inside BookService.addBook...");
+    console.log('Inside BookService.addBook...');
     const user = await User.findById(body.userId);
     if (!user) throw new Error('User not found');
 
@@ -20,11 +20,43 @@ class BookService {
     const book = new Book(body);
     return await book.save();
   };
-  
+
   // Book service for getting all books
   public getAllBooks = async (): Promise<IBook[]> => {
     const books = await Book.find();
     return books;
+  };
+
+  //Book service for updating book
+  public updateBook = async (
+    bookId: string,
+    body: Partial<IBook>
+  ): Promise<IBook | null> => {
+    const user = await User.findById(body.userId);
+    if (!user) throw new Error('User not found');
+    if (user.role !== 'admin')
+      throw new Error('Only admin users can update books');
+    const updatedBook = await Book.findByIdAndUpdate(bookId, body, {
+      new: true
+    });
+    if (!updatedBook) throw new Error('Book not found');
+    return updatedBook;
+  };
+
+  //Book service for deleting book
+  public deleteBook = async (
+    bookId: string,
+    userId: string
+  ): Promise<IBook | null> => {
+    const user = await User.findById(userId);
+    if (!user) throw new Error('User not found');
+    if (user.role !== 'admin')
+      throw new Error('Only admin users can delete books');
+
+    const book = await Book.findById(bookId);
+    if (!book) throw new Error('Book not found');
+    await book.remove();
+    return book;
   };
 }
 
