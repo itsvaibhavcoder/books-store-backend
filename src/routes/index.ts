2e0303104d4c193055e2e4137526f636e9cd5e95
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-13 01:29:33 +0530

    [vaibhav]added:routes for wishlist

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,17 +3,15 @@ const router = express.Router();
 
 import userRoute from './user.route';
 import cartRoutes from './cart.routes';
-/**
- * Function contains Application routes
- *
- * @returns router
- */
+import wishlist from './wishlist.routes'
+
 const routes = (): IRouter => {
   router.get('/', (req, res) => {
     res.json('Welcome vaibhav...');
   });
   router.use('/users', new userRoute().getRoutes());
   router.use('/cart', new cartRoutes().getRoutes());
+  router.use('/wishlist', new wishlist().getRoutes());
   return router;
 };
 
