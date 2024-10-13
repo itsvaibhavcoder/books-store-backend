308e97781c99e19256be1f3a40c0142945507f09
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-13 21:19:38 +0530

    [vaibhav]added: routes for books

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,12 +4,13 @@ const router = express.Router();
 import userRoute from './user.route';
 import cartRoutes from './cart.routes';
 import wishlist from './wishlist.routes'
-
+import bookRoutes from './book.routes'
 const routes = (): IRouter => {
   router.get('/', (req, res) => {
     res.json('Welcome vaibhav...');
   });
   router.use('/users', new userRoute().getRoutes());
+  router.use('/books', new bookRoutes().getRoutes());
   router.use('/cart', new cartRoutes().getRoutes());
   router.use('/wishlist', new wishlist().getRoutes());
   return router;
