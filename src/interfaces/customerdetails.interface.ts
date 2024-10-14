4f3ed15b0b618d1a565fae6e2669bd8c7f1d67b1
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-15 00:32:59 +0530

    [vaibhav]added: added interface for customer

---
+++ src/interfaces/customerdetails.interface.ts
@@ -0,0 +1,18 @@
+import { Document, ObjectId } from 'mongoose';
+
+interface IAddress {
+  _id: ObjectId;
+  addressLine: string;
+  city: string;
+  state: string;
+  postalCode: string;
+  isDefault: boolean;
+}
+
+export interface ICustomer extends Document {
+  _id: ObjectId;
+  email: string;
+  fullName: string;
+  contact: number;
+  addresses: IAddress[];  
+}
