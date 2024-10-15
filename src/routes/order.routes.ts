2ae73e19a62a43667b28742142fe1583656a67d4
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-15 14:27:14 +0530

    [vaibhav]added : routes for order

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,41 +1,23 @@
-// import express, { IRouter } from 'express';
-// import { userAuth } from '../middlewares/auth.middleware';
-// import OrderController from '../controllers/order.controller';
+import express, { IRouter } from 'express';
+import { userAuth } from '../middlewares/auth.middleware';
+import orderController from '../controllers/order.controller';
 
-// class OrderRoutes {
-//   private router = express.Router();
-//   private orderController = new OrderController();
+class OrderRoutes {
+  private OrderController = new orderController();
+  private router = express.Router();
+  constructor() {
+    this.routes();
+  }
 
-//   constructor() {
-//     this.routes();
-//   }
+  private routes = () => {
+    this.router.post('/', userAuth, this.OrderController.createOrder);
+    this.router.get('/', userAuth, this.OrderController.getAllOrders);
+    this.router.get('/:id', userAuth, this.OrderController.getOrderSummary);
+  };
 
-//   private routes = () => {
-//     this.router.post('/create', userAuth, this.orderController.createOrder);
-//     this.router.get('/summary', userAuth, this.orderController.getOrderSummary);
-//   };
+  public getRoutes = (): IRouter => {
+    return this.router;
+  };
+}
 
-//   public getRoutes = (): IRouter => {
-//     return this.router;
-//   };
-// }
-
-// export default OrderRoutes;
-
-
-// // src/routes/order.routes.ts
-
-// import express from 'express';
-// import OrderController from '../controllers/order.controller';
-// import { userAuth } from '../middlewares/auth.middleware';
-
-// const router = express.Router();
-// const orderController = new OrderController();
-
-// router.post('/orders', userAuth, orderController.createOrder);
-// router.get('/orders/:orderId', userAuth, orderController.getOrderById);
-// router.get('/orders/user/:userId', userAuth, orderController.getOrdersByUser);
-// router.patch('/orders/:orderId', userAuth, orderController.updateOrderStatus);
-// router.delete('/orders/:orderId', userAuth, orderController.deleteOrder);
-
-// export default router;
+export default OrderRoutes;
