b470ab0134727f1764124486c6ec67aca5058dd2
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-15 14:26:56 +0530

    [vaibhav]added : routes for order

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,10 @@ const router = express.Router();
 
 import userRoute from './user.route';
 import cartRoutes from './cart.routes';
-import wishlistRoutes from './wishlist.routes'; // Rename to be consistent
+import wishlistRoutes from './wishlist.routes';
 import bookRoutes from './book.routes';
-import CustomerRoutes from './customer.routes'; // Notice capitalization
-
+import CustomerRoutes from './customer.routes'; 
+import OrderRoutes from './order.routes';
 const routes = (): IRouter => {
   router.get('/', (req, res) => {
     res.json('Welcome Vaibhav...');
@@ -17,7 +17,7 @@ const routes = (): IRouter => {
   router.use('/cart', new cartRoutes().getRoutes());
   router.use('/wishlist', new wishlistRoutes().getRoutes()); 
   router.use('/customers', new CustomerRoutes().getRoutes()); 
-  
+  router.use('/orders', new OrderRoutes().getRoutes());
   return router;
 };
 
